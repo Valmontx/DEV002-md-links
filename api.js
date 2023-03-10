aaafdd652a23f3630db8b2f0f2addd36a5c55dee
Valmontx <valeryevnam.h@gmail.com>
2023-03-10 16:14:39 -0500

    Se soluciono la fucion readFile | también se muestran el objeto con las propiedad href, txt,file y stats

--- api.js
+++ api.js
@@ -2,7 +2,9 @@ const path = require('path');
 const fs = require('fs'); // Modulo File System
 const fsp = require('fs/promises');
 const axios = require('axios');
-
+const md = require('markdown-it');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 
 
@@ -18,7 +20,7 @@ const existsPath = (filePath) => {
 //   Saber sí la ruta es relativa ó 'absoluta'  || resolve recorre el sistema de archivos.
 const absolutePath = (filePath) => {
     if (path.isAbsolute(filePath) ? filePath : path.resolve(filePath)) {
-        console.log(absolutePath) // Devuelve la ruta absoluta
+        // Devuelve la ruta absoluta
     }
 }
 
@@ -43,22 +45,22 @@ const readFile = (filePath) => {
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, 'utf-8', (err, data) => {
             if (err) {
-            reject(err)
+                reject(err)
 
-            }else {
+            } else {
                 return resolve(data)
             }
-           
+
         }
         );
 
 
     });
 };
-const pruebaArchivo = './prueba/documentos/ex.md'
+const pruebaArchivo = './prueba/documentos/links.md/file.md'
 readFile(pruebaArchivo)
-.then(contenido => console.log(getLinks(contenido,pruebaArchivo)))
-.catch(err => console.log('Error: No se puede leer el archivo', err ))
+    .then(contenido => console.log(getLinks(contenido, pruebaArchivo)))
+    .catch(err => console.log('Error: No se puede leer el archivo', err))
 
 
 //Devuelve true si es un archivo con la extension .md
@@ -90,86 +92,82 @@ const arrayLinks = (filePath) => {
 
     return arrayMd;
 }
-// console.log(arrayLinks)
 
 // Uniendo ruta con directorio
 const pathJoinDirectory = (directory, filePath) => path.join(directory, filePath);
 
 //  encontrar links - retorna un objeto con las siguientes propiedades href, text , file
-const getLinks = (marKdown , filePath) => { 
+const getLinks = (marKdown, filePath) => {
     const links = []
-    
-    const regex = /\[(.+?)\]\((https?:\/\/[^\s]+)(?: "(.+)")?\)|(https?:\/\/[^\s]+)/ig;
-        //   console.log('funcion',marKdown)
-    let match = regex.exec(marKdown);  /* El método exec() ejecuta una busqueda sobre las coincidencias de una expresión
-           
-           regular en una cadena especifica. 
-           Devuelve el resultado como array, o null .*/
-           
-                for(let i = 0; i < marKdown.length; i++){
-                    if(match !== null){
-                        // console.log('MATCH',match)
-                        links.push({
-                            
-                            href: match[2],
-                            text: match[3],
-                            file: filePath,
-                        })
-                        match= regex.exec(marKdown)
-                    }
-                }
-                return links;
-                
-    };
+    const regex = /(\[(.*?)\])(\((.*?)\))/gim;
+    //   console.log('funcion',marKdown)
+    let match = regex.exec(marKdown);  /* El método exec() ejecuta una busqueda sobre las coincidencias de una expresiónregular en una cadena especifica.    Devuelve el resultado como array, o null .*/
 
-        //   console.log(getLinks({marKdown}))
+    for (let i = 0; i < marKdown.length; i++) {
+        if (match !== null) {
+            // console.log('MATCH',match)
+            links.push({
+
+                href: match[3],
+                text: match[2],
+                file: filePath,
+            })
+            match = regex.exec(marKdown)
+        }
+    }
+    return links;
+
+};
+
+
 
 //  Validar links -  el objeto con las misma propiedades pero agregandole el estado ok o fail
 
 const getLinksValidated = (links) => {
-    const linkValidate = links.map((links) => {
-        
-        return axios.get(links.href)
-            .then((res) => {
-                if (res.status >= 200 && res.status <= 299) { // si la solicitud fue exitosa
-                    console.log('REVISANDO CONTENIDO DE',links)
-                    return {
-                        
-                        href: links.href,
-                        
-                        text: links.text,
-                        file: links.file,
-                        ok: (response.ok)
-                    };
+    const promises = []
+    promises = links.map((link) => axios.get(link.href)
+        .then((res) => {
+            if (res.ok) { // si la solicitud fue exitosa
 
-
-                }
-
-            })
-            .catch((err) => {
-                (err.status >= 400 && err.status >= 499)
                 return {
 
-                    href: links.href,
-                    text: links.text,
-                    file: links.file,
-                    status: res.response ? 404 : "ERROR",
-                    OK: 'Fail',
+                    href: link.href,
 
-
+                    text: link.text,
+                    file: link.file,
+                    ok: (response.ok)
                 };
-            })
-    })
-    return Promise.all(linkValidate);
+
+
+            }
+
+        })
+
+
+        .catch((err) => {
+            (err.status >= 400)
+            return {
+
+                href: link.href,
+                text: link.text,
+                file: link.file,
+                status: res.response ? 404 : "ERROR",
+                ok: 'Fail',
+
+
+            };
+        })
+    )
+    return Promise.all(promises);
 };
 
 
 
 // resultados del stats 
 const validateStatResul = (arrlink) => {
-    const totalLinks = arrlink.map((links) => links.href).size; //---> Esto devolvera el numero de elementos únicos
-    const uniqueLinks = new Set(arrlink.map((links) => links.href)).size;
-    const brokenLinks = arrlink.filter((links) => links.ok === '404')
+    const totalLinks = arrlink.map((link) => link.href).size; //---> Esto devolvera el numero de elementos únicos
+    const uniqueLinks = new Set(arrlink.map((link) => link.href)).size;
+    const brokenLinks = arrlink.filter((link) => link.ok === '404')
 
     return {
         Total: totalLinks.length,
